38b7c590e20990c23edf5a66fca8c5f9a6ad6465
Jason Aricheta <jason.aricheta@gmail.com>
2023-02-09 16:10:50 +1300

    workflow modifications

---
+++ automate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+git add -A
+
+git commit -m "workflow modifications"
+
+git push
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,22 @@
-import { app } from "./server"
-import supertest from 'supertest';
+import { app } from "./server";
+import supertest from "supertest";
 
 const request = supertest(app);
 
 test("get hello world test", async () => {
   const response = await request.get("/");
   expect(response.status).toBe(200);
-  expect(response.body.message).toBe("Hello World! @ GET");   
+  expect(response.body.message).toBe("Hello World! @ GET");
 });
 
 test("post addOne test", async () => {
   const number = 100;
-  const response = await request.post("/").send({number});
+  const response = await request.post("/").send({ number });
   expect(response.status).toBe(200);
   expect(response.body.message).toBe(number + 1);
-})
+});
 
+test("manual test", () => {
+  const num = 100;
+  expect(num).toBe(num + (process.env.TEST_ENV === "FAIL" ? 1 : 0));
+});
